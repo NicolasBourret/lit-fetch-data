b99f2bdaed31a1c09cb78c472bcc8b8eb97db116
Nicolas Bourret <nicolas.bourret48@gmail.com>
2024-11-20 21:52:45 +0100

    first commit

---
+++ src/gql/GetContinent.js
@@ -0,0 +1,7 @@
+export const GetContinent = `
+  {
+    continents {
+      name
+    }
+  }
+`;
---
+++ src/my-element.js
@@ -0,0 +1,48 @@
+import { LitElement, html } from "lit";
+import { Task } from "@lit/task";
+import { GetContinent } from "./gql/GetContinent";
+
+export class MyElement extends LitElement {
+  static properties = {
+    continents: { type: Array },
+  };
+
+  _continentsTask = new Task(this, {
+    task: async ([continents], { signal }) => {
+      const response = await fetch(
+        "https://countries.trevorblades.com/graphql",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ query: GetContinent }),
+          signal,
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+
+      return response.json();
+    },
+    args: () => [this.continents],
+  });
+
+  render() {
+    return this._continentsTask.render({
+      pending: () => html`<p>Loading continents...</p>`,
+      complete: ({ data: { continents } }) => {
+        return html`
+          <ul>
+            ${continents.map((continent) => {
+              return html`<li>${continent.name}</li>`;
+            })}
+          </ul>
+        `;
+      },
+      error: (e) => html`<p>Error: ${e}</p>`,
+    });
+  }
+}
+
+window.customElements.define("my-element", MyElement);
